4d9fab6260becf908dbef4956a6874b772e26be4
Owen Tanner <owen.tanner@informed.com>
2024-12-20 10:43:59 +0000

    Working on debugging why the dockers won't communicate to one another; locally still working

--- EvChargingApp/Program.cs
+++ EvChargingApp/Program.cs
@@ -3,37 +3,35 @@ using EvChargingApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 // Add services to the DI container
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite("Data Source=applications.db")); // SQLite configuration
+    options.UseSqlite("Data Source=applications.db"));
 
-builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
- 
+
+// Add and configure CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", builder =>
+    options.AddPolicy("AllowAll", corsBuilder =>
     {
-        builder.AllowAnyOrigin()
-               .AllowAnyHeader()
-               .AllowAnyMethod();
+        corsBuilder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
     });
 });
 
 var app = builder.Build();
 
-app.UseCors("AllowAll");
+// Configure middleware pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-if (app.Environment.IsDevelopment()) 
-{ 
-    app.UseSwagger(); 
-    app.UseSwaggerUI(); 
-} 
-
-// Middleware pipeline
+app.UseCors("AllowAll"); // Apply the CORS policy
 app.UseAuthorization();
 app.MapControllers();
 
